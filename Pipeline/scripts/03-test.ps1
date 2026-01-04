f3e1b61808281ae455019f08d7e22f0a5b09b237
Murderling <63553040+Murderling@users.noreply.github.com>
2026-01-04 22:15:53 +0100

    added log for pestertests to build/test-results

--- Pipeline/scripts/03-test.ps1
+++ Pipeline/scripts/03-test.ps1
@@ -4,6 +4,6 @@ if (-not (Test-Path $pesterFile)) {
 	Write-Error "Pester test file not found: $pesterFile"
 	exit 2
 }
-Invoke-Pester -Script $pesterFile -Output Detailed
+Invoke-Pester -Script $pesterFile -Output Detailed *> .\build\test-results\pester.log
 exit $LASTEXITCODE
 
